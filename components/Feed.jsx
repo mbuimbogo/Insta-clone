73c9822dd732d5939a77ed89780d6ced29e37f38
mbuimbogo <mbuimbogo99@gmail.com>
2023-03-21 16:08:19 +0300

    populate fake data & map through it in story component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,11 +1,13 @@
+"use client"
 import React from 'react'
+import Stories from './Stories'
 
 export default function Feed() {
   return (
     <main>
       <section>
         {/* stories */}
-
+            <Stories/>
         {/* posts */}
       </section>
 
